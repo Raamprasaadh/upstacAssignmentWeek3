acbdf3f3c650278ff044fbb99102d2318318f419
raamprasaadh <ramprasadhsurya@gmail.com>
2021-08-24 21:23:58 +0530

    completed assignement

---
+++ src/main/java/org/upgrad/upstac/UpstacApplication.java
@@ -0,0 +1,11 @@
+package org.upgrad.upstac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpstacApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UpstacApplication.class, args);
+	}
+}
